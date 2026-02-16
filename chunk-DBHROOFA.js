1535b699d34fdb19132e382473639a4387fdfcc3
Murat Selim <murat@Murats-MacBook-Air.local>
2026-02-16 17:27:54 +0300

    Auto-generated commit

--- chunk-DBHROOFA.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-ELK26WZG.js";import"./chunk-A35KAJYI.js";import"./chunk-IU75DD4N.js";export{a as AdminModule};
---
+++ chunk-PIA2D5A3.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-62232ADJ.js";import"./chunk-A35KAJYI.js";import"./chunk-IU75DD4N.js";export{a as AdminModule};
